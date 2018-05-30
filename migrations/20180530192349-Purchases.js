4114f813e98ac88a14fdd8929c4f9a2e09c8516e
Sarah Spikes <sarah@effectivealtruism.org>
2018-05-30 16:20:10 -0700

    add migrations with db-migrate

---
+++ migrations/20180530192349-Purchases.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.createTable('Purchases', {
+    id: { type: 'int', primaryKey: true },
+    product_id: {
+      type: 'int',
+      foreignKey: {
+        name: 'product_id_fk',
+        table: 'Products',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT'
+        },
+        mapping: 'id'
+      }
+    },
+    user_id: {
+      type: 'int',
+      foreignKey: {
+        name: 'user_id_fk',
+        table: 'Users',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT'
+        },
+        mapping: 'id'
+      }
+    },
+    recipient_id: {
+      type: 'int',
+      foreignKey: {
+        name: 'recipient_id_fk',
+        table: 'Recipients',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT'
+        },
+        mapping: 'id'
+      }
+    }
+  }, callback);
+  return null;
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
