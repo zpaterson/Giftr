7a9da7fc78d0a5f9adfb2cbee79800322496da3a
Lian <lianthompson@gmail.com>
2018-06-26 11:16:59 -0700

    products display

--- client/src/components/products/products.js
+++ client/src/components/products/products.js
@@ -13,31 +13,33 @@ class Products extends Component {
         this.getProducts();
     }
 
-    getProducts = _ => {
+    getProducts = () => {
         console.log("getting products");
         // looks for /products in server.js
         fetch('/products')
             .then((res) => {
                 return res.json();
             })
-            .then(res => this.setState({ products: res.data[0].title }))
+            .then(res => this.setState({ products: res.data }))
             .catch(err => console.error(err))
-        // console.log(data);
-        //   console.log(this.state.products);
+         //console.log(data);
+           console.log(this.state.products);
     }
-    // renderProduct = ({ product_id, name }) =>
-    //     <li
-    //         key={product_id}>{name}
-    //     </li>
+
 
     render() {
         const { products } = this.state;
+        const renderProduct = (title) =>
+        <li
+            key={title}>{title}
+        </li>
         return (
             <div>
                 {/* <Signup auth={this.props.auth} history={this.props.history} /> */}
                     <div className="Products">
-                        {/* {products.map(products =>this.renderProduct)} */}
-                        <p>{products}</p>
+                    <ul>
+                        {products.map(product =>renderProduct(product.TITLE))}
+                    </ul>
                     </div>
             </div>
         );
